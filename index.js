7354dd687c31eacdaa5d5b5dc5a792350ca2b062
Brandi Burnell <brandiburnell6@gmail.com>
2023-10-29 13:14:30 -0600

    passed all tests

--- index.js
+++ index.js
@@ -51,16 +51,131 @@ function myReduce(collection, callback, acc) {
         internalCollection = collection;
     }
 
-    console.log(acc);
-    // change acc to first value if it does not exist
+    // // change acc to first value if it does not exist
+    let didaccChange = false;
+    let i = 0;
     if (acc === undefined) {
         acc = internalCollection[0];
+        i = 1;
     }
-    console.log(acc);
 
-    // iterate through array and adjust the value of acc
-    for (const element of internalCollection) {
-        acc = callback(acc, element, internalCollection);
+    for (i; i < internalCollection.length; i++) {
+        acc = callback(acc, internalCollection[i], internalCollection);
     }
+
     return acc;
+}
+
+// look through each value in collection and return the first one that passes the 
+// test 
+function myFind(collection, predicate) {
+    // change object to array if collection is object
+    let internalCollection;
+    if (!Array.isArray(collection)) {
+        internalCollection = Object.values(collection);
+    }
+    else {
+        internalCollection = collection;
+    }
+
+    for (const element of internalCollection) {
+        if (predicate(element)) {
+            return element;
+        }
+    }
+}
+
+// look through collection and return all values that pass truth test
+function myFilter(collection, predicate) {
+    // change object to array if collection is object
+    let internalCollection;
+    if (!Array.isArray(collection)) {
+        internalCollection = Object.values(collection);
+    }
+    else {
+        internalCollection = collection;
+    }
+
+    const filteredArray = [];
+    for (const element of internalCollection) {
+        if (predicate(element)) {
+            filteredArray.push(element);
+        }
+    }  
+
+    return filteredArray;
+}
+
+// returns the number of values in the array
+function mySize(collection) {
+    // change object to array if collection is object
+    let internalCollection;
+    if (!Array.isArray(collection)) {
+        internalCollection = Object.values(collection);
+    }
+    else {
+        internalCollection = collection;
+    }
+    
+    let sizeCount = 0;
+    for (const element of internalCollection) {
+        sizeCount++;
+    }
+
+    return sizeCount;
+}
+
+// returns the first element of the array
+function myFirst(array, n) {
+    // check to see if [n] exists
+    if (n === undefined) {
+        return array[0];
+    }
+    else {
+        let returnArray = [];
+        for (let i = 0; i < n; i++) {
+            returnArray.push(array[i]);
+        }
+        return returnArray;
+    }
+}
+
+// returns the first element of the array
+function myLast(array, n) {
+    // check to see if [n] exists
+    if (n === undefined) {
+        return array[array.length - 1];
+    }
+    else {
+        let returnArray = [];
+        // // console.log(array.length);
+        // console.log(n);
+        // console.log(array);
+        for (let i = array.length; i > (array.length - n); i--) {
+            //console.log(i);
+            returnArray.unshift(array[i - 1]);
+        }
+        // console.log(returnArray);
+        return returnArray;
+    }
+}
+
+// retrieve all names of the objects keys
+function myKeys(object) {
+    let keyNames = [];
+    // console.log(object);
+    for (const key in object) {
+        keyNames.push(key);
+        // console.log(object[key]);
+    }
+    return keyNames;
+}
+
+// returns all values of object
+function myValues(object) {
+    let values = [];
+    for (const key in object) {
+        values.push(object[key]);
+    }
+    return values;
 }
